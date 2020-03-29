810abb525fe541455aab6cd06bbea82db439d859
Louis Korczowski <louis.korczowski@gmail.com>
2020-03-29 13:14:38 +0200

    refactoring preprocess as module

--- covid19lib/preprocess.py
+++ covid19lib/preprocess.py
@@ -3,8 +3,9 @@ import pandas as pd
 #import matplotlib.pyplot as plt
 import argparse
 from pathlib import Path
+import os
 
-age_intervals = [0,19,39,59,74,200]
+age_intervals = [0, 19, 39, 59, 74, 200]
 dtypes = {'annee_comptabilisation': 'int64',
           'sexe': 'int64',
           'age': 'int64', 
@@ -12,17 +13,22 @@ dtypes = {'annee_comptabilisation': 'int64',
           'nb_deces': 'int64', 
           'date_deces': 'str'}
 
+
 def load_preprocess_df(df_filename):
     df = pd.read_csv(df_filename, sep=';', dtype=dtypes)
     df['date_deces'] = pd.to_datetime(df['date_deces'])
-    df = df[df['date_deces']>='1990-01-01']
+    df = df[df['date_deces'] >= '1990-01-01']
+
     # To take age into account, we will create a new feature `age_bin` based on INSEE standard: (0:19, 20:39, 40:59, 60:74, 75+)
     df['age_bin'] = pd.cut(df['age'], age_intervals, include_lowest=True, labels=np.array(age_intervals[:-1])+1)
+
     # Aggregate by age_bin rather than age
     df = df.groupby(["date_deces", "age_bin", "sexe", "departement_deces"], as_index=False)["nb_deces"].sum()
-    df['nb_deces'] = df['nb_deces'].astype(pd.Int64Dtype()) # pandas int that handle NaN...
+    df['nb_deces'] = df['nb_deces'].astype(pd.Int64Dtype())  # pandas int that handle NaN...
     df['departement_deces'] = df['departement_deces'].astype('category')
-    return(df)
+
+    return df
+
 
 # On développe ensuite la variable `date_deces` pour enrichir les données :
 def add_features(df):
@@ -31,24 +37,35 @@ def add_features(df):
     df['month'] = df['date_deces'].dt.month
     df['year'] = df['date_deces'].dt.year
     df['weeknumber'] = df['date_deces'].dt.week
-    return(df)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f",
-                        "--filename",
-                        default='INSEE_deces_2010_2019',
-                        help="The raw csv to process.")
-#    parser.add_argument("-g",
-#                        "--num_gpus",
-#                        type=int,
-#                        default=NUM_GPUS,
-#                        help="The number of GPUs.")
-    args = parser.parse_args()
-    filename = args.filename
-    
-    Path("./preprocessed_data/").mkdir(parents=True, exist_ok=True)
+    return df
 
-    df = load_preprocess_df('./data/'+filename+'.csv')
+
+def preprocess_data(filename,
+                    inputdir="./data/",
+                    outputdir="./preprocessed_data/"):
+    """preprocess a file from ./data/ and save it as pickle in  ./preprocessed_data/
+
+    Parameters
+    ----------
+    filename : str
+        name of the .csv file to preprocess without the extension (e.g. "test" for file `test.csv`).
+    inputdir : str (default: "./data/")
+        folder path containing the file
+    outputdir: str (default: "./preprocessed_data/")
+`       folder path for the output preprocessed file
+
+    Returns
+    -------
+    output_filepath: str
+        the signal
+    """
+
+    Path(outputdir).mkdir(parents=True, exist_ok=True)
+
+    df = load_preprocess_df(os.path.join(inputdir, filename+'.csv'))
     df = add_features(df)
-    df.to_pickle('./preprocessed_data/'+filename+'.pkl')
+    output_filepath = os.path.join(outputdir, filename+'.pkl')
+    df.to_pickle(output_filepath)
+
+    return output_filepath
