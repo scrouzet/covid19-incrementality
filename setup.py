e9466b586a8412fe35c11b144e14457d3e6e5a16
Sebastien Crouzet <scrouzet@brainchipinc.com>
2020-03-30 16:44:14 +0200

    setup.py multipurpose extract and process

--- setup.py
+++ setup.py
@@ -10,14 +10,15 @@ if __name__ == '__main__':
     datafolder = "data" + os.path.sep
     outputdir = "preprocessed_data" + os.path.sep
     tmpdir = "tmp" + os.path.sep
-
+    
     for file in os.listdir(datafolder):
-        if file.endswith(".zip"):
+        if file.endswith(".zip") & file.startswith("INSEE_deces_"):
             extract_archives(os.path.join(datafolder, file), outputdir=tmpdir)
-            
-    for file in os.listdir(datafolder):
-        if file.startswith("INSEE_deces_"):
             preprocess_data(str.split(file, sep=".")[0], inputdir=tmpdir, outputdir=outputdir)
+
+        if file.endswith(".zip") & file.startswith("other_structure_TBD_"):
+            extract_archives(os.path.join(datafolder, file), outputdir=tmpdir)
+            #preprocess_data_2020(str.split(file, sep=".")[0], inputdir=tmpdir, outputdir=outputdir)
 
 
     [os.remove(file) for file in glob.glob(os.path.join(tmpdir, "*.csv"))]
